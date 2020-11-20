de80593d1aa6533737ecf05939c1eef96769cd8f
Augustine <amenezes927@gmail.com>
2020-11-20 16:49:29 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+
 import './App.css';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 import { DISHES } from './shared/dishes';
 
 class App extends Component {
@@ -12,17 +12,13 @@ class App extends Component {
       dishes: DISHES
     };
   }
-  render(){
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+
 }
 export default App;
