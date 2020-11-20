f07bc47fb44754f76f460f59a02cba7d5303ea46
Augustine <amenezes927@gmail.com>
2020-11-20 21:10:41 +0530

    Single page applications part-1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 import { DISHES } from './shared/dishes';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -14,9 +15,12 @@ class App extends Component {
   }
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
+      
     );
   }
 
